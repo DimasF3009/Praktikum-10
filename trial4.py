f1af37866473ef4d7464f505ece9b65f60d5a5f5
DimasF3009 <115356128+DimasF3009@users.noreply.github.com>
2023-01-17 01:46:28 +0700

    Add files via upload

---
+++ trial4.py
@@ -0,0 +1,8 @@
+txt = "Hello world"
+print("Karakter variabel txt =",len(txt))
+print(txt[10])
+print(txt[2:5])
+print(txt.replace(" ",""))
+print(txt.upper())
+print(txt.lower())
+print(txt.replace("H","J"))
